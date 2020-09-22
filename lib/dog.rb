a18ee7ecfe6a286930e72f4adb39d99bd82e9799
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-22 18:09:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 # Your code goes here!
 
 class Dog 
-  def name=(dog_name)
+  def .name=(dog_name)
     this_dogs_name = dog_name
   end 
   
-  def name
+  def .name
     this_dogs_name
   end 
   
-  def bark 
+  def .bark 
     puts "woof!"
   end 
 end
